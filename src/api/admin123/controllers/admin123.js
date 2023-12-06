5a08bd7618da181a6df0b3b388df458eee2c4ec5
Sdcse <suva.ewp@gmail.com>
2023-12-06 04:03:55 -0800

    #first-commit

---
+++ src/api/admin123/controllers/admin123.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * admin123 controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::admin123.admin123');
---
+++ src/api/admin123/routes/admin123.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * admin123 router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::admin123.admin123');
